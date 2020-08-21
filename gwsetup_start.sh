fe2c439390177af2df3702f5d4a7792e0dce8aad
pascal <sarl@hubrecht.info>
2020-08-21 07:52:59 +0200

    cleanup code

--- gwsetup_start.sh
+++ gwsetup_start.sh
@@ -8,7 +8,6 @@ LANGUAGE=fr
 
 
 # Start gwsetup
-# cd ${BASESDIR} && ${BINDIR}/gwsetup -p 2316 -gd ${BINDIR} -lang ${LANGUAGE} -bindir ${BINDIR} -only ${BASESDIR}/gwsetup_only.txt 2>&1 | tee -a ${LOGSDIR}/gwsetup.log
 cd ${BASESDIR} && ${BINDIR}/gwsetup -p 2316 -gd ${BINDIR} -lang ${LANGUAGE} -bindir ${BINDIR} -only ${BASESDIR}/gwsetup_only.txt 2>&1 | tee -a ${LOGSDIR}/gwsetup.log
 
 
